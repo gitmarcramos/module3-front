3ea72bdcbbb5b4b1cc29960899171f2e5b819ee0
Marc Ramos <marcramos.webdev@gmail.com>
2021-11-18 16:09:19 +0100

    VITE BASE URL and not REACT base ulr

--- src/api/handler.js
+++ src/api/handler.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
+
 const APIHandler = (() => {
   return axios.create({
-    baseURL: "http://localhost:5550",
-    // baseURL: import.meta.env.REACT_APP_BACKEND_URL,
+    // baseURL: "http://localhost:5550",
+    baseURL: import.meta.env.VITE_BACKEND_URL,
     withCredentials: true,
   });
 })();
